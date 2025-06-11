ac61548518cdbd3ea5a46bad6fcbe3d0f3d0097e
Dhanya-WW <Dhanya.Radhakrishna@WinWire.com>
2025-06-11 11:27:37 +0530

    my first commit

---
+++ src/Content.js
@@ -0,0 +1,40 @@
+
+ import  {useState} from 'react';
+
+
+const Content = () => {
+   const [name, setName] = useState('Jathin');
+   const [count, setCount] = useState(0);
+
+    const handleNameChange = () => {
+        const names =['Jathin','Jagan','Dhanush'];
+        const int = Math.floor(Math.random() * 3);
+        setName(names[int]);
+  }
+  const handleClick =() =>
+  {
+    setCount(count + 1);
+    setCount(count + 1);
+    console.log(count)
+  }
+  const handleClick2 = () => {
+    console.log(count)
+}
+ const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+}
+  return (
+    <main>
+        <p onDoubleClick={handleClick}>
+            Hello {name}!
+        </p>
+        <button onClick={handleNameChange}>Change Name</button>
+        <button onClick={handleClick}>Click it</button>
+        <button onClick={handleClick2}>Click it</button>
+
+        <button onClick={(e) => handleClick3(e)}>Click it</button>
+    </main>
+  )
+}
+
+export default Content
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+
+
+const Footer = props => {
+    const today = new Date();
+  return (
+    <footer>
+        <p> Copyright &copy; {today.getFullYear()}</p>
+    </footer>
+  )
+}
+
+
+export default Footer
